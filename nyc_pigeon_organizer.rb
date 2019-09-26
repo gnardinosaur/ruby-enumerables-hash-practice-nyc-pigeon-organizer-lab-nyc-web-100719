6102184e601c82076becd606e5b3f2f9a8e861fa
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-26 22:28:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,14 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, attribute_hash| #color, gender, or lives
     attribute_hash.each do |key, value| #values are arrays of names 
+      p "#{key}"
       value.each do |name|
         pigeon_list[name] = {
           :color => [],
           :gender => [],
           :lives => []
         }
+        # if the name is present add that attribute as a string to 
       end
     end
   end
