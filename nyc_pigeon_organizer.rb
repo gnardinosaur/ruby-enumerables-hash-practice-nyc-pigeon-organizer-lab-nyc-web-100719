6eb10fa08802b087f8a54cb137b1a834ee1190f3
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-26 22:15:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, attribute_hash| #color, gender, or lives
+    attribute_hash.each do |key, value| #values are arrays of names 
+      value.each do |name|
+        pigeon_list[name] = {
+          :color => [],
+          :gender => [],
+          :lives => []
+        }
+      end
+    end
+  end
+  p pigeon_list
 end
+
+
